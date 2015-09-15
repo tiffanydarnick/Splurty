e0a7c220b1bafe362bdc286a748ee04800d17505
Tiffany <tdarnick@outlook.com>
2015-09-15 19:05:01 +0000

    header1

---
+++ test/controllers/quots_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuotsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/quots_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class QuotsHelperTest < ActionView::TestCase
+end
