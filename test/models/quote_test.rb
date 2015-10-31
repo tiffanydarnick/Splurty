4629a8dce672f465ec477d6d428f298d89112b72
tiffanydarnick <tdarnick@outlook.com>
2015-10-31 20:29:02 +0000

    tdd4

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
  	
  	test "unique_tag" do
- 		quote = Quote.create(:author=>'Hamish', :saying=>'No you are not.')
+ 		quote = Quote.create(:author=>'Hamish Blake', :saying=>'No you are not.')
  		expected = 'HA#' + quote.id.to_s
  		actual = quote.unique_tag
  		assert_equal expected, actual
